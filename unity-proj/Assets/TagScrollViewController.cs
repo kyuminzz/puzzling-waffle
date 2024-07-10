46fe34cc174b74b83a3eab23c8f0d084a55ebb51
jongwoo <izowooi@hotmail.com>
2024-07-10 21:01:21 +0900

    text 사용을 위한 폰트 추가
    font : jebrainmono, 나눔

---
+++ unity-proj/Assets/TagScrollViewController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class TagScrollViewController : MonoBehaviour
+{
+    private ScrollRect scrollRect;
+    public GameObject uiPrefab;
+    public float horizontalSpace = 20f; // 수평 간격을 의미하도록 변수명 변경
+    public List<RectTransform> uiObjects = new List<RectTransform>();
+    
+    void Start()
+    {
+        scrollRect = GetComponent<ScrollRect>();
+        LoadTags();
+    }
+
+    void LoadTags()
+    {
+        Debug.Log("Starting to load tags");
+        
+        AddNewUIObject();
+        AddNewUIObject();
+        AddNewUIObject();
+    }
+
+    public RectTransform AddNewUIObject()
+    {
+        var newUi = Instantiate(uiPrefab, scrollRect.content).GetComponent<RectTransform>();
+        
+        float x = 0f;
+        if (uiObjects.Count > 0)
+        {
+            RectTransform lastUi = uiObjects[uiObjects.Count - 1];
+            x = lastUi.anchoredPosition.x + lastUi.sizeDelta.x + horizontalSpace;
+        }
+        
+        newUi.anchoredPosition = new Vector2(x, 0f);
+        uiObjects.Add(newUi);
+
+        // 콘텐츠 크기 조정
+        float contentWidth = x + newUi.sizeDelta.x;
+        scrollRect.content.sizeDelta = new Vector2(contentWidth, scrollRect.content.sizeDelta.y);
+
+        return newUi;
+    }
+}
