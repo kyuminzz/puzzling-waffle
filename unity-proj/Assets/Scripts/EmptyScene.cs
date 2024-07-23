cdafc1175ce3fee35518ab36ccd997c12d3ac0a8
jongwoo <izowooi@hotmail.com>
2024-07-23 23:15:36 +0900

    테스트를 위한 씬 추가

---
+++ unity-proj/Assets/Scripts/EmptyScene.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyScene : MonoBehaviour
+{
+    private StageProgressData _stageProgressData;
+
+    void Start()
+    {
+        _stageProgressData = StageProgressManager.Instance.LoadData();
+    }
+    
+    public void Test_Stage_2_2()
+    {
+        SaveProgress(2, 2, new List<PuzzlePiecePosition>
+        {
+            new PuzzlePiecePosition(1, 2),
+            new PuzzlePiecePosition(3, 4),
+        });
+    }
+    public void Test_Stage_2_1()
+    {
+        SaveProgress(2, 2, new List<PuzzlePiecePosition>
+        {
+            new PuzzlePiecePosition(1, 2)
+        });
+    }
+    
+    public void Test_StageClear()
+    {
+        MarkStageAsCleared(1, 4, 123.45f);
+    }
+    
+    public void Test_Show()
+    {
+        foreach (var progress in _stageProgressData.InProgressStages)
+        {
+            Debug.Log($"StageIndex: {progress.StageIndex}, Difficulty: {progress.Difficulty}");
+            foreach (var piece in progress.Pieces)
+            {
+                Debug.Log($"Piece: ({piece.X}, {piece.Y})");
+            }
+        }
+
+        foreach (var clearedStage in _stageProgressData.ClearedStages)
+        {
+            Debug.Log($"StageIndex: {clearedStage.StageIndex}, Difficulty: {clearedStage.Difficulty}, ClearTime: {clearedStage.ClearTime}");
+        }
+    }
+
+    public void Test_Remove()
+    {
+        StageProgressManager.Instance.ClearData();
+        _stageProgressData = StageProgressManager.Instance.LoadData();
+    }
+
+    void SaveProgress(int stageIndex, int difficulty, List<PuzzlePiecePosition> pieces)
+    {
+        PuzzleProgressInfo progressInfo = new PuzzleProgressInfo(stageIndex, difficulty);
+        progressInfo.Pieces.AddRange(pieces);
+        _stageProgressData.InProgressStages.Add(progressInfo);
+        StageProgressManager.Instance.SaveData(_stageProgressData);
+    }
+
+    void MarkStageAsCleared(int stageIndex, int difficulty, float clearTime)
+    {
+        PuzzleClearInfo clearInfo = new PuzzleClearInfo(stageIndex, difficulty, clearTime);
+        _stageProgressData.ClearedStages.Add(clearInfo);
+        StageProgressManager.Instance.SaveData(_stageProgressData);
+    }
+}
